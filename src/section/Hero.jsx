d5efaf2919772ed030645afaf538b3a54443572c
OmkarPatil-10 <omkarpatil9935@gamil.com>
2024-11-24 16:56:27 +0530

    first commit

---
+++ src/section/Hero.jsx
@@ -0,0 +1,21 @@
+
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className="w-full flex
+      xl:flex-row flex-col 
+      justify-center min-h-screen
+      gap-10 max-container 
+      border-2 border-red-500 p-2"
+    >
+
+      <div className="relative 
+      xl:w-2/5 flex flex-col
+      justify-center items-start"></div>
+
+    </section>
+  )
+}
+
+export default Hero
